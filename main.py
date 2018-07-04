6eca958ca7e6bb31a2e59a4a7ff829b2f36b4aa1
Andreas Ehrlund <andreas@Andreass-Air.local>
2018-07-05 01:01:41 +0200

    refactoring into a module

--- main.py
+++ main.py
@@ -7,36 +7,16 @@ __author__ = "Andreas Ehrlund"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import sys
-import requests
-from requests_oauthlib import OAuth1Session, OAuth1
-import yaml
-
+from pymkm import PyMKM
 
 def main():
     """ Main entry point of the app """
-    print(">>> Welcome to pymkm.")
+    print(">>> Welcome to a pymkm test app.")
 
-    try:
-        config=yaml.load(open('config.yml'))
-    except Exception as error:
-        print("You must copy config_template.yml to config.yml and populate the fields.")
-        sys.exit(0)
-    
-    url = 'https://www.mkmapi.eu/ws/v1.1/account'
-
-    mkmService = OAuth1Session(
-        config['app_token'],
-        client_secret=config['app_secret'],
-        resource_owner_key=config['access_token'],
-        resource_owner_secret=config['access_token_secret'],
-        realm=url
-        )
-    
-    r = mkmService.get(url)
-    print(r.request.headers)
-    print('---')
-    print(r.content)
+    api = PyMKM()
+    response = api.get_account()
+    print(response)
+    print(response.content)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
---
+++ pymkm.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Andreas Ehrlund"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+import sys
+import requests
+from requests_oauthlib import OAuth1Session, OAuth1
+import yaml
+
+class PyMKM:
+    config = None
+    mkmService = None
+
+    def __init__(self):
+        print(">>> Loading config file.")
+        try:
+            self.config=yaml.load(open('config.yml'))
+        except Exception as error:
+            print("You must copy config_template.yml to config.yml and populate the fields.")
+            sys.exit(0)
+
+    def get_account(self):
+        url = 'https://www.mkmapi.eu/ws/v1.1/output.json/account'
+        if (self.config != None):
+            self.mkmService = OAuth1Session(
+            self.config['app_token'],
+            client_secret=self.config['app_secret'],
+            resource_owner_key=self.config['access_token'],
+            resource_owner_secret=self.config['access_token_secret'],
+            realm=url
+            )
+
+        r = self.mkmService.get(url)
+        return r
