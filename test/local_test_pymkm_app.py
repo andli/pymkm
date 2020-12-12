88428cbb3e135d6fc924a3e377cad83201a4b080
Andreas Ehrlund <a.ehrlund@gmail.com>
2020-12-12 21:25:48 +0100

    local testing WIP

--- test/local_test_pymkm_app.py
+++ test/local_test_pymkm_app.py
@@ -14,22 +14,22 @@ from test.local_test_common import LocalTestCommon
 
 
 class TestPyMkmApp(LocalTestCommon):
-    # @patch("sys.stdout", new_callable=io.StringIO)
-    # @patch("builtins.input", side_effect=["0"])  # 1 e e y 0
-    ## TODO: instead of patching, trigger each depending on the stdout
-    # def test_app_starts(self, mock_input, mock_stdout, *args):
-    #    self.assertEquals(True, True)
-    #    app = PyMkmApp()
-    #    app.start(self.parsed_args)
-    #    self.assertRegex(mock_stdout.getvalue(), r"╭─── PyMKM")
-
     @patch("sys.stdout", new_callable=io.StringIO)
-    @patch("builtins.input", side_effect=["1", "", "", "y", "0"])
-    def test_stock_update(self, mock_input, mock_stdout, *args):
+    @patch("builtins.input", side_effect=["0"])
+    def test_app_starts(self, mock_input, mock_stdout, *args):
         self.assertEquals(True, True)
         app = PyMkmApp()
         app.start(self.parsed_args)
-        self.assertRegex(mock_stdout.getvalue(), r"Stock updated")
+        self.assertRegex(mock_stdout.getvalue(), r"╭─── PyMKM")
+
+    # @patch("sys.stdout", new_callable=io.StringIO)
+    # @patch("builtins.input", side_effect=["1", "", "", "y", "0"])
+    ## TODO: instead of patching, trigger each depending on the stdout
+    # def test_stock_update(self, mock_input, mock_stdout, *args):
+    #    self.assertEquals(True, True)
+    #    app = PyMkmApp()
+    #    app.start(self.parsed_args)
+    #    self.assertRegex(mock_stdout.getvalue(), r"Stock updated")
 
 
 if __name__ == "__main__":
