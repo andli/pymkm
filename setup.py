778cf017ee005f42b6ca184f038de0f04b2df6e1
Andreas Ehrlund <a.ehrlund@gmail.com>
2021-01-18 15:24:14 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pymkm',
+    version='2.3.0',
+    packages=['test', 'pymkm'],
+    url='https://github.com/andli/pymkm/',
+    license='MIT',
+    author='Andreas Ehrlund',
+    author_email='a.ehrlund@gmail.com',
+    description='Python wrapper for the cardmarket.com API. Also contains a working app with price updating of your personal stock, csv import etc.'
+)
