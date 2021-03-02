7864979757c18a5c08449271a2f8cef614c6af0f
Andreas Ehrlund <a.ehrlund@gmail.com>
2021-03-02 15:11:26 +0100

    disabled a unit test

--- test/local_test_pymkm_app.py
+++ test/local_test_pymkm_app.py
@@ -18,17 +18,20 @@ class TestPyMkmApp(LocalTestCommon):
         app.start(self.parsed_args)
         self.assertRegex(mock_stdout.getvalue(), r"╭─── PyMKM")
 
-    @patch("sys.stdout", new_callable=io.StringIO)
-    @patch("builtins.input", side_effect=["backupstock", "y", "restorestock", "y", "0"])
-    def test_stock_update(self, mock_stdin, mock_stdout, *args):
-        self.assertEqual(True, True)
-        app = PyMkmApp()
-        app.start(self.parsed_args)
+    # This thest does live changes to the account, disabled for now
+    # @patch("sys.stdout", new_callable=io.StringIO)
+    # @patch("builtins.input", side_effect=["backupstock", "y", "restorestock", "y", "0"])
+    # def test_stock_update(self, mock_stdin, mock_stdout, *args):
+    #    self.assertEqual(True, True)
+    #    app = PyMkmApp()
+    #    app.start(self.parsed_args)
 
-        out = mock_stdout.getvalue()
-        self.assertRegex(out, r"API calls used today")
-        # self.assertRegex(out, r"Restoring cached stock...")
-        # self.assertRegex(out, r"Done.")
+
+#
+#    out = mock_stdout.getvalue()
+#    self.assertRegex(out, r"API calls used today")
+#    # self.assertRegex(out, r"Restoring cached stock...")
+#    # self.assertRegex(out, r"Done.")
 
 
 if __name__ == "__main__":
